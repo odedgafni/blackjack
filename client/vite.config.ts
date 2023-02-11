a307454423484badec948c834e23235f004a7396
odedgafni <gafnio@gmail.com>
2023-02-11 10:52:54 +0200

    open browser on default

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react({
     include: "**/*.tsx"
-  })]
+  })],
+  server: {
+    port: 5000,
+    open: '/'
+  }
 })
